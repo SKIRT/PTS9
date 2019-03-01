922e287dd181f6599261b977b68fd9159b1499e7
Peter Camps <peter.camps@ugent.be>
2019-03-01 09:54:50 +0100

    add final polarization plot and finetune some of the other plots

--- visual/plotpolarization.py
+++ visual/plotpolarization.py
@@ -15,8 +15,9 @@
 
 import logging
 import matplotlib.pyplot as plt
-import matplotlib.lines
 import matplotlib.colors
+import matplotlib.lines
+import matplotlib.patches
 import numpy as np
 import pts.simulation as sm
 import pts.utils as ut
@@ -39,19 +40,19 @@ import pts.utils as ut
 #   - plotLinMap: whether to plot a linear polarization map (binned degree and angle); True or False.
 #   - plotDegMap: whether to plot a linear polarization degree map; True or False.
 #   - plotDegAvg: whether to plot the y-axis averaged linear polarization degree for all x pixels; True or False.
-#   - plotCirMap: whether to plot a circular polarization map; True, False or None for automatic.
+#   - plotCirMap: whether to plot a circular polarization map; True or False.
 #   - wavelength: an astropy quantity or a sequence of astropy quantities specifying the wavelength(s) for which to
 #     create the plot, or the string 'all' to create a plot for all wavelengths in the data cube, or None to use the
 #     first frame in the data cube.
 #   - binSize: the number of pixels in each bin, in horizontal and vertical directions.
-#   - polScale: (degree, length) to set the scale of the polarization degree; use Nones for automatic scale.
+#   - degreeScale: the maximum polarization degree to be visualized, or None for automatic scale.
 #   - decades: number of surface brightness decades (dex) shown in the background image; default is 5.
 #   - outDirPath: string or Pathlib.Path object that specifies (overrides) the output directory.
 #   - figSize: the horizontal and vertical size of the output figure in inch; default is 6x6 inch.
 #   - interactive: whether to leave figures open (True) or save them to file (False).
 #
 def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAvg=False, plotCirMap=False,
-                     wavelength=None, binSize=(7,7), polScale=(None,None), decades=5,
+                     wavelength=None, binSize=(7,7), degreeScale=None, decades=5,
                      outDirPath=None, figSize=(8, 6), interactive=None):
 
     # loop over all applicable instruments
@@ -89,11 +90,9 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
         dropY = orLenY % binY
         startY = dropY//2
 
-        # construct arrays with central bin positions scaled to actual axes values
+        # construct arrays with central bin positions in pixel coordinates
         posX = np.arange(startX - 0.5 + binX / 2.0, orLenX - dropX + startX - 0.5, binX)
         posY = np.arange(startY - 0.5 + binY / 2.0, orLenY - dropY + startY - 0.5, binY)
-        posX = xmin + posX * (xmax-xmin)/orLenX
-        posY = ymin + posY * (ymax-ymin)/orLenY
 
         # determine the appropriate wavelength index or indices
         if wavelength is None:
@@ -116,9 +115,6 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
             U = Us[:,:,index].T.value
             V = Vs[:,:,index].T.value
 
-            # if plotCirMap is None, we'll detect circular polarization for this frame during binning
-            plotCirMapThis = plotCirMap
-
             # perform the actual binning
             binnedI = np.zeros((len(posY),len(posX)))
             binnedQ = np.zeros((len(posY),len(posX)))
@@ -130,14 +126,6 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
                     binnedQ[y,x] = np.sum(Q[startY+binY*y : startY+binY*(y+1) , startX+binX*x : startX+binX*(x+1)])
                     binnedU[y,x] = np.sum(U[startY+binY*y : startY+binY*(y+1) , startX+binX*x : startX+binX*(x+1)])
                     binnedV[y,x] = np.sum(V[startY+binY*y : startY+binY*(y+1) , startX+binX*x : startX+binX*(x+1)])
-                    # we only need circular polarization relative to total Intensity
-                    binnedV[y,x] /= binnedI[y,x]
-                    # if needed, detect circular polarization
-                    if plotCirMapThis is None:
-                        stdV = np.nanstd(V[startY+binY*y : startY+binY*(y+1) , startX+binX*x : startX+binX*(x+1)]/
-                                         I[startY+binY*y : startY+binY*(y+1) , startX+binX*x : startX+binX*(x+1)])
-                        if stdV == 0: stdV = 1 # if we only have one polarized pixel in the bins, stdV is undefined
-                        if np.abs(binnedV[y,x])/stdV > 2: plotCirMapThis = True
 
             # -----------------------------------------------------------------
 
@@ -181,11 +169,9 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
                 degreeLD[degreeLD<charDegree/50] = np.NaN
 
                 # determine the scaling so that the longest arrows do not to overlap with neighboring arrows
-                degreeScale, lengthScale = polScale
                 if degreeScale is None:
-                    degreeScale = _roundUp(charDegree, slack=0.0001)
-                if lengthScale is None:
-                    lengthScale = 1/(degreeScale*2.2)/max(float(len(posX))/figSize[0], float(len(posY))/figSize[1])
+                    degreeScale = _roundUp(charDegree)
+                lengthScale = 2.2 * degreeScale * max(float(len(posX))/figSize[0], float(len(posY))/figSize[1])
                 key = "{:.3g}%".format(100 * degreeScale)
 
                 # compute the polarization angle
@@ -195,11 +181,11 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
                 xPolarization = -degreeLD*np.sin(angle) #For angle = 0: North & x=0, For angle = 90deg: West & x=-1
                 yPolarization =  degreeLD*np.cos(angle) #For angle = 0: North & y=1, For angle = 90deg: West & y=0
 
-                # plot the vector field
-                X,Y = np.meshgrid(posX, posY)
+                # plot the vector field (scale positions to data coordinates)
+                X,Y = np.meshgrid(xmin + posX * (xmax - xmin) / orLenX, ymin + posY * (ymax - ymin) / orLenY)
                 quiverPlot = ax.quiver(X,Y, xPolarization, yPolarization, pivot='middle', units='inches',
-                                        angles = 'xy', scale = 1/lengthScale, scale_units = 'inches', headwidth=0,
-                                        headlength=1, headaxislength=1, minlength = 0.8, width = 0.02)
+                                        angles='xy', scale=lengthScale, scale_units='inches', headwidth=0,
+                                        headlength=1, headaxislength=1, minlength=0.8, width=0.02)
                 ax.quiverkey(quiverPlot, 0.85, 0.02, degreeScale, key, coordinates='axes', labelpos='E')
 
                 # if not in interactive mode, save the figure; otherwise leave it open
@@ -223,16 +209,16 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
                 ax.set_ylabel("y" + sm.latexForUnit(ygrid), fontsize='large')
 
                 # calculate polarization degree for each pixel, in percent
+                # set degree to zero for pixels with very low intensity
+                cutmask = I < ( np.nanmax(I[I < 1e6*np.nanmedian(np.unique(I))]) / 10**decades )
                 degreeHD = np.sqrt(Q**2 + U**2)
-                degreeHD[degreeHD>0] /= I[degreeHD>0]
+                degreeHD[~cutmask] /= I[~cutmask]
+                degreeHD[cutmask] = 0
                 degreeHD *= 100
 
-                # set degree to zero for pixels with very low intensity
-                Icutoff = np.nanmax(I[I < 1e6*np.nanmedian(np.unique(I))]) / 10**decades
-                degreeHD[I < Icutoff] = 0
-
                 # plot the image and the corresponding color bar
-                normalizer = matplotlib.colors.Normalize(vmin=0, vmax=np.percentile(degreeHD, 99))
+                vmax = degreeScale if degreeScale is not None else np.percentile(degreeHD, 99)
+                normalizer = matplotlib.colors.Normalize(vmin=0, vmax=vmax)
                 backPlot = ax.imshow(degreeHD, norm=normalizer, cmap='plasma', extent=extent,
                                      aspect='equal', interpolation='bicubic', origin='lower')
                 plt.colorbar(backPlot, ax=ax).set_label("Linear polarization degree (%)" + " @ " +  wavelatex,
@@ -256,6 +242,7 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
                 degreeHD /= np.average(I,axis = 0)
                 ax.plot(xgrid.value, degreeHD*100)
                 ax.set_xlim(xmin, xmax)
+                ax.set_ylim(0, degreeScale)
                 ax.set_title("{}   {}".format(insname, wavelatex), fontsize='large')
                 ax.set_xlabel("x" + sm.latexForUnit(xgrid), fontsize='large')
                 ax.set_ylabel('Average linear polarization degree (%)', fontsize='large')
@@ -271,14 +258,62 @@ def plotPolarization(simulation, *, plotLinMap=True, plotDegMap=False, plotDegAv
             # -----------------------------------------------------------------
 
             # plot a circular polarization map
-            if plotCirMapThis:
-                pass
+            if plotCirMap:
+                fig, ax = plt.subplots(ncols=1, nrows=1, figsize=figSize)
+
+                # configure the axes
+                ax.set_xlim(xmin, xmax)
+                ax.set_ylim(ymin, ymax)
+                ax.set_xlabel("x" + sm.latexForUnit(xgrid), fontsize='large')
+                ax.set_ylabel("y" + sm.latexForUnit(ygrid), fontsize='large')
+
+                # determine intensity range for the background image, ignoring pixels with outrageously high flux
+                Ib = I.copy()
+                highmask = Ib > 1e6*np.nanmedian(np.unique(Ib))
+                vmax = np.nanmax(Ib[~highmask])
+                Ib[highmask] = vmax
+                vmin = vmax / 10**decades
+
+                # plot the background image and the corresponding color bar
+                normalizer = matplotlib.colors.LogNorm(vmin, vmax)
+                cmap = plt.get_cmap('PuRd')
+                cmap.set_under('w')
+                backPlot = ax.imshow(Ib, norm=normalizer, cmap=cmap, extent=extent,
+                                     aspect='equal', interpolation='bicubic', origin='lower')
+                cbarlabel = sm.latexForSpectralFlux(Is) + sm.latexForUnit(Is) + " @ " +  wavelatex
+                plt.colorbar(backPlot, ax=ax).set_label(cbarlabel, fontsize='large')
+
+                # compute the circular polarization degree
+                degreeLD = binnedV.copy()
+                degreeLD[binnedI>0] /= binnedI[binnedI>0]
+
+                # determine the scaling and add legend
+                if degreeScale is None:
+                    degreeScale = _roundUp(np.percentile(np.abs(degreeLD), 99))
+                lengthScale = 0.7/max(len(posX),len(posY))
+                _circArrow(ax, 0.84-lengthScale/2, 0.01+lengthScale/2, lengthScale)
+                key = r'$+{} \%$'.format(100*degreeScale)
+                ax.text(0.85, 0.01+lengthScale/2, key, transform=ax.transAxes, ha='left', va='center')
+
+                # actual plotting
+                for x in range(len(posX)):
+                    for y in range(len(posY)):
+                        if np.isfinite(degreeLD[y,x]) and abs(degreeLD[y,x]) > degreeScale/50:
+                            _circArrow(ax, posX[x]/orLenX, posY[y]/orLenY, degreeLD[y,x]/degreeScale*lengthScale)
+
+                # if not in interactive mode, save the figure; otherwise leave it open
+                if not ut.interactive(interactive):
+                    saveFilePath = ut.savePath(filepath, ".pdf", outDirPath=outDirPath,
+                                               outFileName = "{}_{}_polcirmap.pdf".format(insname, wavename))
+                    plt.savefig(saveFilePath, bbox_inches='tight', pad_inches=0.25)
+                    plt.close()
+                    logging.info("Created {}".format(saveFilePath))
 
 # -----------------------------------------------------------------
 
 ## This helper function rounds up a given number (away from zero) to one significant digit, in a logarithmic fashion.
 # The \em slack argument allows a number just larger than the nearest limit to be clamped to that limit.
-def _roundUp(x, slack = 0.):
+def _roundUp(x, slack = 0.01):
     x *= 1.-slack
     signX = np.sign(x)
     x = np.abs(x)
@@ -289,3 +324,20 @@ def _roundUp(x, slack = 0.):
     if mant <= 4: return signX*4*dec
     if mant <= 5: return signX*5*dec
     return signX*10*dec
+
+## This helper function draws a circular polarization arrow in the given matplotlib axes.
+def _circArrow(ax, posx, posy, size):
+    # add the arc
+    lw = 1
+    oa = 33
+    ax.add_artist(matplotlib.patches.Arc((posx,posy), size, size, theta1=oa, theta2=-oa,
+                                         lw=lw, capstyle='round',
+                                         color='k', zorder = 10, clip_on=False, transform=ax.transAxes))
+    # add the arrow
+    x = posx + size / 2 * np.cos(oa / 180 * np.pi)
+    y = posy - abs(size) / 2 * np.sin(oa / 180 * np.pi)
+    ax.add_artist(matplotlib.lines.Line2D((x - 0.5 * size / 2, x, x), (y, y, y - 0.5 * abs(size) / 2),
+                                          lw=lw, solid_capstyle='round', dash_capstyle='round',
+                                          color='k', zorder = 10, clip_on=False, transform=ax.transAxes))
+
+# -----------------------------------------------------------------
