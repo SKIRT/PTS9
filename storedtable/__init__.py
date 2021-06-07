f5ddeef839b12969de101905cc3b5419e08ab153
Peter Camps <peter.camps@ugent.be>
2021-06-07 15:33:02 +0200

    Add support for SKIRT stored columns format

--- storedtable/__init__.py
+++ storedtable/__init__.py
@@ -9,13 +9,14 @@
 #  Package initialization file
 # -----------------------------------------------------------------
 
-## \package pts.storedtable Support for converting resources for SKIRT to "SKIRT stored table" format
+## \package pts.storedtable Support for handling SKIRT stored table and stored columns formats
 #
-# This package contains classes and functions to help convert data used as resources by SKIRT from
-# their original format (whatever that may be) to the proprietary "SKIRT stored table" format. This
-# format is optimized for processing with SKIRT and is its mandatory resource format.
+# This package contains classes and functions to help convert data used by SKIRT as a built-in
+# resource or as an input file from their original format (whatever that may be) to the proprietary
+# "SKIRT stored table" or "SKIRT stored columns" file formats.
 #
 
 from .conversionspec import createConversionSpecs, ConversionSpecs, ConversionSpec
-from .io import listStoredTableInfo, readStoredTable, writeStoredTable
+from .io import listStoredTableInfo, readStoredTable, writeStoredTable, \
+                listStoredColumnsInfo, readStoredColumns, writeStoredColumns
 from .tokenizedfile import TokenizedFile
